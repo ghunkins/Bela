1b2be206bfca320553725abdc815111b63ea8b32
ghunkky <ghunk@dhcp-10-5-3-78.wireless.rochester.edu>
2017-03-24 10:21:20 -0400

    Basic FFT example
    Only comment modifications

---
+++ build/main.d
@@ -0,0 +1,20 @@
+/root/Bela/projects/AudioMVR/build/main.o: \
+  /root/Bela/projects/AudioMVR/main.cpp \
+  /root/Bela/projects/AudioMVR/SampleData.h include/Bela.h \
+  /usr/xenomai/include/rtdk.h /usr/xenomai/include/xeno_config.h \
+  include/digital_gpio_mapping.h include/GPIOcontrol.h \
+  include/Utilities.h
+
+/root/Bela/projects/AudioMVR/SampleData.h:
+
+include/Bela.h:
+
+/usr/xenomai/include/rtdk.h:
+
+/usr/xenomai/include/xeno_config.h:
+
+include/digital_gpio_mapping.h:
+
+include/GPIOcontrol.h:
+
+include/Utilities.h:
---
+++ build/render.d
@@ -0,0 +1,88 @@
+/root/Bela/projects/AudioMVR/build/render.o: \
+  /root/Bela/projects/AudioMVR/render.cpp include/Bela.h \
+  /usr/xenomai/include/rtdk.h /usr/xenomai/include/xeno_config.h \
+  include/digital_gpio_mapping.h include/GPIOcontrol.h \
+  include/Utilities.h include/ne10/NE10.h include/ne10/NE10_types.h \
+  include/ne10/NE10_macros.h include/ne10/NE10_init.h \
+  include/ne10/NE10_math.h include/ne10/NE10_dsp.h \
+  include/ne10/NE10_imgproc.h include/ne10/NE10_physics.h \
+  /root/Bela/projects/AudioMVR/SampleData.h include/Midi.h \
+  /usr/xenomai/include/native/pipe.h /usr/xenomai/include/nucleus/pipe.h \
+  /usr/xenomai/include/nucleus/heap.h \
+  /usr/xenomai/include/nucleus/queue.h \
+  /usr/xenomai/include/nucleus/types.h \
+  /usr/xenomai/include/asm/xenomai/system.h \
+  /usr/xenomai/include/nucleus/system.h \
+  /usr/xenomai/include/asm/xenomai/atomic.h \
+  /usr/xenomai/include/asm/xenomai/features.h \
+  /usr/xenomai/include/asm-generic/xenomai/features.h \
+  /usr/xenomai/include/nucleus/trace.h \
+  /usr/xenomai/include/asm-generic/xenomai/syscall.h \
+  /usr/xenomai/include/asm/xenomai/tsc.h \
+  /usr/xenomai/include/nucleus/compiler.h \
+  /usr/xenomai/include/nucleus/assert.h \
+  /usr/xenomai/include/native/types.h
+
+include/Bela.h:
+
+/usr/xenomai/include/rtdk.h:
+
+/usr/xenomai/include/xeno_config.h:
+
+include/digital_gpio_mapping.h:
+
+include/GPIOcontrol.h:
+
+include/Utilities.h:
+
+include/ne10/NE10.h:
+
+include/ne10/NE10_types.h:
+
+include/ne10/NE10_macros.h:
+
+include/ne10/NE10_init.h:
+
+include/ne10/NE10_math.h:
+
+include/ne10/NE10_dsp.h:
+
+include/ne10/NE10_imgproc.h:
+
+include/ne10/NE10_physics.h:
+
+/root/Bela/projects/AudioMVR/SampleData.h:
+
+include/Midi.h:
+
+/usr/xenomai/include/native/pipe.h:
+
+/usr/xenomai/include/nucleus/pipe.h:
+
+/usr/xenomai/include/nucleus/heap.h:
+
+/usr/xenomai/include/nucleus/queue.h:
+
+/usr/xenomai/include/nucleus/types.h:
+
+/usr/xenomai/include/asm/xenomai/system.h:
+
+/usr/xenomai/include/nucleus/system.h:
+
+/usr/xenomai/include/asm/xenomai/atomic.h:
+
+/usr/xenomai/include/asm/xenomai/features.h:
+
+/usr/xenomai/include/asm-generic/xenomai/features.h:
+
+/usr/xenomai/include/nucleus/trace.h:
+
+/usr/xenomai/include/asm-generic/xenomai/syscall.h:
+
+/usr/xenomai/include/asm/xenomai/tsc.h:
+
+/usr/xenomai/include/nucleus/compiler.h:
+
+/usr/xenomai/include/nucleus/assert.h:
+
+/usr/xenomai/include/native/types.h:
